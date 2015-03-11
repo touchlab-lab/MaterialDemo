67e7b83a22df36bef0491f5e7c18d99e1105d68d
eoji <eoji11592@gmail.com>
2015-03-11 12:46:57 -0400

    Added drawer resources, base styling, and transitions. Added home activity with drawer listing our main topics.

---
+++ app/src/main/java/co/touchlab/materialdemo/HomeActivity.java
@@ -0,0 +1,228 @@
+package co.touchlab.materialdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class HomeActivity extends ActionBarActivity
+{
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence title;
+    private int          currentSelectedPosition;
+    private ListView     drawerListView;
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        title = getTitle();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                                                                       R.string.drawer_open,
+                                                                       R.string.drawer_close)
+        {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view)
+            {
+                super.onDrawerClosed(view);
+                //getActionBar().setTitle(title);
+                //invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView)
+            {
+                super.onDrawerOpened(drawerView);
+                //getActionBar().setTitle(mDrawerTitle);
+                //invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        drawerLayout.setDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        setUpDrawer();
+    }
+
+    private void setUpDrawer()
+    {
+        drawerListView = (ListView) findViewById(R.id.drawer_list);
+        drawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                selectItem(position);
+            }
+        });
+
+        ArrayList<String> topics = new ArrayList<>();
+        topics.add("RecyclerViews");
+        topics.add("CardViews");
+        //cool stuff with elevation
+        topics.add("Shadows and Clipping");
+        //scene transitions and view animations
+        topics.add("Animations");
+        topics.add("Palette");
+        drawerListView.setAdapter(new ArrayAdapter<>(getSupportActionBar().getThemedContext(),
+                                                           android.R.layout.simple_list_item_activated_1,
+                                                           android.R.id.text1, topics));
+
+    }
+
+    private void selectItem(int position)
+    {
+        currentSelectedPosition = position;
+        if(drawerListView != null)
+        {
+            drawerListView.setItemChecked(position, true);
+        }
+        if(drawerLayout != null)
+        {
+            drawerLayout.closeDrawer(drawerListView);
+        }
+        onNavigationDrawerItemSelected(position);
+
+    }
+
+    public void onNavigationDrawerItemSelected(int position)
+    {
+//        update the main content by replacing fragments
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                       .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                       .commit();
+    }
+
+    public void onSectionAttached(int number)
+    {
+        switch(number)
+        {
+            case 1:
+                title = getString(R.string.title_section1);
+                break;
+            case 2:
+                title = getString(R.string.title_section2);
+                break;
+            case 3:
+                title = getString(R.string.title_section3);
+                break;
+        }
+    }
+
+    public void restoreActionBar()
+    {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(title);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        if(!drawerLayout.isDrawerOpen(drawerListView))
+        {
+            getMenuInflater().inflate(R.menu.home, menu);
+            restoreActionBar();
+            return true;
+        }
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if(id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment
+    {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber)
+        {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment()
+        {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(String.valueOf(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity)
+        {
+            super.onAttach(activity);
+            ((HomeActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+
+}
